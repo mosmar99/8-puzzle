71f84413d27a6d66b90198267fdd5926efc124ba
Linkan42 <sali20jt@student.ju.se>
2021-09-27 17:39:31 +0200

    task 1-4 done and tested, exkl. 'type State'

--- lab2_Linus.hs
+++ lab2_Linus.hs
@@ -1,13 +1,37 @@
+import Data.List
+import Data.Maybe
+
 --Task 1
 type Board = [[Int]]
-type Position = (Int,Int)
---type State = 
+type Position = (Int,Int) -- (x,y)
+--type State = (Board,) ??
 
 --Task 2
---- TODO explain and understand
+{-  Create instance of class "Action", using Eq typeclass, with 4 memberfunctions each
+    using the 'show' function to display a string depending on which function is called-}
 data Action = L | U | R | D deriving Eq
 instance Show Action where
     show L = "Left"
     show U = "Up"
     show R = "Right"
     show D = "Down"
+
+--Task 3
+solution :: Board
+solution = [[1,2,3],[4,5,6],[7,8,0]]
+
+isSolved :: Board -> Bool   --tested
+isSolved board = concat board == concat solution
+
+--Task 4
+findEmpty :: Board -> Position  --tested
+findEmpty board
+    | index <= 2 = (xPos,0)
+    | index <= 5 = (xPos,1)
+    | index <= 8 = (xPos,2) 
+        where index = fromJust $ findIndex (== 0) (concat board) --always within [0,8]
+              xPos = rem index 3 --similar method for yPos??
+
+--Task 5
+replace :: Position -> Int -> Board -> Board
+replace pos n board
