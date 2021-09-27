71ed75c2613a4ea9faf5e8a447a8176d6739ffd3
MahmutOsmanovic <47375043+MahmutOsmanovic@users.noreply.github.com>
2021-09-28 00:53:18 +0200

    changed types, completed all tasks untill task 7, to be continued

--- labTwo.hs
+++ labTwo.hs
@@ -1,24 +1,38 @@
--- Task 1
-type Board = [[Int]] -- revise
-type Position = (Int,Int) -- revise
-type State = Int
+import Data.List
+import Data.Maybe
 
--- task 2
--- left, down, right, up, derives the characteristics of Eq
+type Board = [Int] 
+type Position = Int
+type State = Int -- revise
+
+{-  Create instance of class "Action", using Eq typeclass, with 4 memberfunctions each
+    using the 'show' function to display a string depending on which function is called-}
 data Action = L | U | R | D deriving Eq 
-
---  creates an instance of type Action with four methods using the show function
-instance Show Action where -- 
+instance Show Action where 
     show L = "Left"
     show U = "Up"
     show R = "Right"
     show D = "Down"
 
--- task 3
 solution :: Board
-solution = [[1,2,3], [4,5,6], [7,8,-1]]
+solution = [1,2,3,4,5,6,7,8,-1]
 
--- isSolved board = concatmap filter (a -> Bool) ([a])
--- idea: concat both maps, check if each values is identical in sol and isSolved
+isSolved :: Board -> Bool
+isSolved board = board == solution
 
+findEmpty :: Board -> Position
+findEmpty board = fromJust(elemIndex (-1) board)
 
+replace :: Position -> Int -> Board -> Board
+replace idx n board = leftPart ++ (n:rightPart) where (leftPart, (_:rightPart)) = splitAt idx board
+
+swap :: Position -> Position -> Board -> Board
+swap idx1 idx2 board = replace idx1 (board !! idx2) (replace idx2 (board !! idx1) board)
+
+makeMove :: Board -> Action -> Board -- moves "-1" within the 3x3 plane
+makeMove board move
+    | let idx = findEmpty board, move == "Left" = swap (idx) (idx-1) board  
+    | let idx = findEmpty board, move == "Up" = swap (idx) (idx-3) board  
+    | let idx = findEmpty board, move == "Right" = swap (idx) (idx+1) board  
+    | let idx = findEmpty board, move == "Down" = swap (idx) (idx+3) board
+    -- continue 
