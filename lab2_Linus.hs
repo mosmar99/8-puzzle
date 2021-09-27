aa614132e4b061b01e5d368eaf718fa116549cac
Linkan42 <sali20jt@student.ju.se>
2021-09-27 19:34:13 +0200

    task 6 done and tested

--- lab2_Linus.hs
+++ lab2_Linus.hs
@@ -1,12 +1,10 @@
 import Data.List
 import Data.Maybe
 
---Task 1
 type Board = [[Int]]
 type Position = (Int,Int) -- (x,y)
 --type State = (Board,) ??
 
---Task 2
 {-  Create instance of class "Action", using Eq typeclass, with 4 memberfunctions each
     using the 'show' function to display a string depending on which function is called-}
 data Action = L | U | R | D deriving Eq
@@ -16,14 +14,12 @@ instance Show Action where
     show R = "Right"
     show D = "Down"
 
---Task 3
 solution :: Board
 solution = [[1,2,3],[4,5,6],[7,8,0]]
 
 isSolved :: Board -> Bool   --tested
 isSolved board = concat board == concat solution
 
---Task 4
 findEmpty :: Board -> Position  --tested
 findEmpty board
     | index <= 2 = (xPos,0)
@@ -32,20 +28,23 @@ findEmpty board
         where index = fromJust $ findIndex (== 0) (concat board) --always within [0,8]
               xPos = rem index 3 --similar method for yPos??
 
---Task 5
+getValue :: Position -> Board -> Int
+getValue (x,y) board = (concat board) !! (x + 3 * y)
+
 replace :: Position -> Int -> Board -> Board    --tested
-replace (x,y) n board =
-    let flatBoard = concat board
-    in reverseConcat (changeValue (flatBoard !! (x + 3 * y)) n flatBoard)
-        where 
-            reverseConcat board =   --reverse the effect of concat
-                let row1 = take 3 board
-                    row2 = take 3 $ drop 3 board
-                    row3 = take 3 $ drop 6 board
-                in [row1,row2,row3]
-            changeValue value newValue list     --change 'value' to newValue in a list
-                | first == [] = foldr (:) [newValue] $ init second
-                | otherwise = (foldr (:) [newValue] (init first)) ++ second
-                    where tuple = splitAt value list
-                          first = fst tuple
-                          second = snd tuple
+replace pos n board = reverseConcat (changeValue (getValue pos board) n (concat board))
+    where 
+        reverseConcat board =   --reverse the effect of concat
+            let row1 = take 3 board
+                row2 = take 3 $ drop 3 board
+                row3 = take 3 $ drop 6 board
+            in [row1,row2,row3]
+        changeValue value newValue list     --change 'value' to 'newValue' in a list
+            | first == [] = foldr (:) [newValue] $ init second
+            | otherwise = (foldr (:) [newValue] (init first)) ++ second
+                where tuple = splitAt value list
+                      first = fst tuple
+                      second = snd tuple
+
+swap :: Position -> Position -> Board -> Board  --tested
+swap pos1 pos2 board = replace pos2 (getValue pos1 board) $ replace pos1 (getValue pos2 board) board
