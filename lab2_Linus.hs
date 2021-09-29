38dfd04879b7096f82a1cdc7aafa09ab7fa855c5
Linkan42 <sali20jt@student.ju.se>
2021-09-29 11:28:38 +0200

    made type Board = [Int] and reworked relevant methods, task 7 done

--- lab2_Linus.hs
+++ lab2_Linus.hs
@@ -1,8 +1,8 @@
 import Data.List
 import Data.Maybe
 
-type Board = [[Int]]
-type Position = (Int,Int) -- (x,y)
+type Board = [Int]
+type Position = Int --[0,8]
 type State = (Board,[Action])
 
 {-  Custom data type 'Action' which can be either L, U, R or D, using Eq typeclass to print
@@ -16,43 +16,26 @@ instance Show Action where
     show D = "Down"
 
 solution :: Board
-solution = [[1,2,3],[4,5,6],[7,8,0]]
+solution = [1,2,3,4,5,6,7,8,0]
+testBoard :: Board  --temporary board to test functions
+testBoard = [2,7,0,8,5,1,3,6,4]
 
 isSolved :: Board -> Bool   --tested
-isSolved board = concat board == concat solution
+isSolved board = board == solution
 
 findEmpty :: Board -> Position  --tested
-findEmpty board
-    | index <= 2 = (xPos,0)
-    | index <= 5 = (xPos,1)
-    | index <= 8 = (xPos,2) 
-        where index = fromJust $ findIndex (== 0) (concat board) --always within [0,8]
-              xPos = rem index 3 --similar method for yPos??
-
-getValue :: Position -> Board -> Int
-getValue (x,y) board = (concat board) !! (x + 3 * y)
+findEmpty board = fromJust $ findIndex (== 0) board --always within [0,8]
 
 replace :: Position -> Int -> Board -> Board    --tested
-replace pos n board = reverseConcat (changeValue (getValue pos board) n (concat board))
-    where 
-        reverseConcat board =   --reverse the effect of concat
-            let row1 = take 3 board
-                row2 = take 3 $ drop 3 board
-                row3 = take 3 $ drop 6 board
-            in [row1,row2,row3]
-        changeValue value newValue list     --change 'value' to 'newValue' in a list
-            | first == [] = foldr (:) [newValue] $ init second
-            | otherwise = (foldr (:) [newValue] (init first)) ++ second
-                where tuple = splitAt value list
-                      first = fst tuple
-                      second = snd tuple
+replace ix newVal board = fst ++ newVal:ys where (fst,(_:ys)) = splitAt ix board
 
 swap :: Position -> Position -> Board -> Board  --tested
-swap pos1 pos2 board = replace pos2 (getValue pos1 board) $ replace pos1 (getValue pos2 board) board
+swap ix1 ix2 board = replace ix2 (board !! ix1) $ replace ix1 (board !! ix2) board
 
-makeMove :: Board -> Action -> [Board]
-makeMove board action = case action of U -> swap zeroPos (zeroPosX,zeroPosY - 1) board   --up
-                                       D -> swap zeroPos (zeroPosX,zeroPosY + 1) board   --down
-                                       L -> swap zeroPos (zeroPosX - 1,zeroPosY) board   --left
-                                       R -> swap zeroPos (zeroPosX + 1,zeroPosY) board   --right
-                                    where zeroPos@(zeroPosX,zeroPosY) = findEmpty board
+makeMove :: Board -> Action -> [Board]  --tested
+makeMove board action = if not $ elem ix validPositions then [] else (swap ix (ix + offset) board) : []
+    where ix = findEmpty board
+          (validPositions, offset) = case action of U -> ([3..8],(-3))  --all valid positions for their respective actions and the offset to make that action
+                                                    D -> ([0..5],3)
+                                                    L -> ([1,2,4,5,7,8],(-1))
+                                                    R -> ([0,1,3,4,6,7],1)
