f49d903777ebdc2d138c1909203d451211145b3a
MahmutOsmanovic <47375043+MahmutOsmanovic@users.noreply.github.com>
2021-10-01 12:21:03 +0200

    written allfutures, updated possibleSol

--- labTwo.hs
+++ labTwo.hs
@@ -1,9 +1,9 @@
 import Data.List
 import Data.Maybe
 
-type Board = [Int]
-type Position = Int
-type State = Int -- revise
+type Board = [Int] -- list from 0 to 8
+type Position = Int -- index in list
+type State = (Board, [Action])
 
 {-  Create instance of class "Action", using Eq typeclass, with 4 memberfunctions each
     using the 'show' function to display a string depending on which function is called-}
@@ -24,7 +24,7 @@ findEmpty :: Board -> Position
 findEmpty board = fromJust(elemIndex (-1) board)
 
 replace :: Position -> Int -> Board -> Board
-replace idx n board = leftPart ++ (n:rightPart) where (leftPart, (_:rightPart)) = splitAt idx board
+replace idx n board = leftPart ++ (n:rightPart) where (leftPart, _:rightPart) = splitAt idx board
 
 swap :: Position -> Position -> Board -> Board
 swap idx1 idx2 board = replace idx1 (board !! idx2) (replace idx2 (board !! idx1) board)
@@ -32,9 +32,20 @@ swap idx1 idx2 board = replace idx1 (board !! idx2) (replace idx2 (board !! idx1
 makeMove :: Board -> Action -> Board -- moves "-1" within the 3x3 plane
 makeMove board move = if idx `elem` idxs then [] else swap idx (idx + off) board
     where
-      idx = findEmpty board
-      (idxs, off) = case move of
-        L -> ([0, 3, 6], -1)
-        R -> ([2, 5, 8], 1)
-        U -> ([0, 1, 2], -3)
-        D -> ([6, 7, 8], 3)
+        idx = findEmpty board
+        (idxs, off) = case move of
+            L -> ([0, 3, 6], -1)
+            R -> ([2, 5, 8], 1)
+            U -> ([0, 1, 2], -3)
+            D -> ([6, 7, 8], 3)
+
+allFutures :: State -> [State]
+allFutures state = filter (not . null . fst) $ map func options
+    where
+        (currBoard, prevMoves) = state
+        idx = findEmpty currBoard
+        func ltr = (makeMove currBoard ltr, ltr : prevMoves)
+        options = [L,R,U,D]
+
+possibleSolutions :: Board -> [[State]]
+possibleSolutions board = [(board,[])] : [concatMap allFutures x | x <- possibleSolutions board]
